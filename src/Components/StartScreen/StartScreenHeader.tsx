98b621b1e90688f9ad03cd30c89758ebe9d2cc62
Larysa Yavorska <nottombraider@Larysas-MacBook-Air.local>
2021-02-19 19:10:39 +0100

    feat: create game logo and add 2 button controls for music and sound

--- src/Components/StartScreen/StartScreenHeader.tsx
+++ src/Components/StartScreen/StartScreenHeader.tsx
@@ -1,12 +1,20 @@
 import "../../styles/utility-styles.css";
+import "./StartScreenStyles/StartScreenHeaderStyles.css";
 
 export const StartScreenHeader = () => {
   return (
-    <header className="flex align-center space-around">
-      <h1>Flag Quiz</h1>
+    <header className="flex align-center space-around start-screen-header">
+      <h1 className="start-screen-header__text-logo">
+        <span className="start-screen-header__sub-text">one more</span>
+        Flag Quiz <span className="game-header-icon">&#127918;</span>
+      </h1>
       <div>
-        <button>Music</button>
-        <button>Sounds</button>
+        <button className="music-header-icon" aria-label="Music button">
+          <span>&#128264;</span>
+        </button>
+        <button className="sounds-header-icon" aria-label="Sounds button">
+          <span>&#127926;</span>
+        </button>
       </div>
     </header>
   );
