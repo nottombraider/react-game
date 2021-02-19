1e878d0ea2ef35c0518c5d26341e44ea74fabc85
Larysa Yavorska <nottombraider@Larysas-MacBook-Air.local>
2021-02-19 17:27:28 +0100

    feat: add basic page layout

---
+++ src/FlagQuizGame.tsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from "react";
+import { StartScreenHeader } from "./Components/StartScreen/StartScreenHeader";
+import "./styles/utility-styles.css";
+import StartScreen from "./Components/StartScreen/StartSreen";
+
+const FlagQuizGame = () => {
+  return (
+    <div>
+      <StartScreen />
+    </div>
+  );
+};
+
+export default FlagQuizGame;
