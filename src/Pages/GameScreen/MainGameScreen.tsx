dd03e3b68580d13363486997cc0812c918d504ff
Larysa Yavorska <nottombraider@Larysas-MacBook-Air.local>
2021-02-21 18:50:32 +0100

    feat: create main game field

--- src/Pages/GameScreen/MainGameScreen.tsx
+++ src/Pages/GameScreen/MainGameScreen.tsx
@@ -1,13 +1,29 @@
 import { RouteComponentProps } from "@reach/router";
 import { DefaultLayout } from "Layouts/DefaultLayout";
 import { FunctionComponent } from "react";
+import { getVariants } from "utils";
+import { useCountryFlags } from "./useCountryFlags";
 
 export const MainGameScreen: FunctionComponent<RouteComponentProps> = () => {
+  const [countryFlags, isLoading] = useCountryFlags();
+  const countryFlagsVariants = getVariants(countryFlags, 4);
+  const [correctAnswer] = countryFlagsVariants;
+
+  if (isLoading) return <h1>Loading game...</h1>;
   return (
     <DefaultLayout>
-      <div>
-        <h1>GAME SCREEN</h1>
-      </div>
+      <main>
+        <figure>
+          <img src={correctAnswer.flag} alt="flag" />
+        </figure>
+        <ul>
+          {countryFlagsVariants.map((countryFlagVariant) => (
+            <li key={countryFlagVariant.name}>
+              <button>{countryFlagVariant.name}</button>
+            </li>
+          ))}
+        </ul>
+      </main>
     </DefaultLayout>
   );
 };
