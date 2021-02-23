870ba53c07505cc18cf717c82844f70c1077db0c
Larysa Yavorska <nottombraider@Larysas-MacBook-Air.local>
2021-02-23 14:17:05 +0100

    fix: implement correct picking possible answers

--- src/utils/getVariants.ts
+++ src/utils/getVariants.ts
@@ -4,11 +4,12 @@ export const getVariants = <ItemType extends {}>(
   items: Array<ItemType>,
   amount: number
 ) => {
+  const possibleAnswers = [...items];
   const results: Array<ItemType> = [];
 
   while (amount--) {
-    const randomItemIndex = getRandomNumber(0, items.length - 1);
-    const result = items[randomItemIndex];
+    const randomItemIndex = getRandomNumber(0, possibleAnswers.length - 1);
+    const result = possibleAnswers[randomItemIndex];
     results.push(result);
     items.splice(randomItemIndex, 1);
   }
