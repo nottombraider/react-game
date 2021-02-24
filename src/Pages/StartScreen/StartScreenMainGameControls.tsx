4bf74c276d0af5d34f4387c8bb24868c9e3fcb41
Larysa Yavorska <nottombraider@Larysas-MacBook-Air.local>
2021-02-24 19:52:02 +0100

    fix: implement correct paths use

--- src/Pages/StartScreen/StartScreenMainGameControls.tsx
+++ src/Pages/StartScreen/StartScreenMainGameControls.tsx
@@ -1,11 +1,12 @@
 import { useNavigate } from "@reach/router";
+import { RoutPaths } from "Pages/routes";
 
 export const StartScreenMainGameControls = () => {
   const navigate = useNavigate();
 
   return (
     <main className="start-screen-main flex column align-center space-around">
-      <button onClick={() => navigate("/game")}>Start Game</button>
+      <button onClick={() => navigate(RoutPaths.GameScreen)}>Start Game</button>
       {/* <button>Settings</button> */}
     </main>
   );
--- src/Pages/routes.tsx
+++ src/Pages/routes.tsx
@@ -1,12 +1,20 @@
 import { Router } from "@reach/router";
+import { GameOver } from "./EndScreens/GameOver";
 import { MainGameScreen } from "./GameScreen/MainGameScreen";
 import { StartScreen } from "./StartScreen/StartScreen";
+
+export enum RoutPaths {
+  StartScreen = "/",
+  GameScreen = "/game",
+  GameOver = "/game-over",
+}
 
 export const Routes = () => {
   return (
     <Router>
-      <StartScreen path="/" />
-      <MainGameScreen path="/game" />
+      <StartScreen path={RoutPaths.StartScreen} />
+      <MainGameScreen path={RoutPaths.GameScreen} />
+      <GameOver path={RoutPaths.GameOver} />
     </Router>
   );
 };
