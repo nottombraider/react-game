e6fe762f9e3d9bc438e093ee0baf74faf338aaa7
Larysa Yavorska <nottombraider@Larysas-MacBook-Air.local>
2021-02-24 19:50:19 +0100

    feat: add redirect to main screen

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -2,13 +2,18 @@ import React from "react";
 import AudioPlayControlButton from "Pages/StartScreen/AudioPlayControlButton";
 import "../styles/utility-styles.css";
 import "./styles-Header.css";
+import { navigate } from "@reach/router";
+import { RoutPaths } from "Pages/routes";
 
 const AUDIO_CONTROL_ICONS: string[] = ["music", "sounds"];
 
 export const Header = () => {
   return (
     <header className="header flex align-center space-around ">
-      <div className="header__logo">
+      <div
+        className="header__logo"
+        onClick={() => navigate(RoutPaths.StartScreen)}
+      >
         <span className="header__sub-text">one more</span>
         <h1 className="header__text-logo">
           Flag Quiz <span className="game-header-icon">&#127918;</span>
