856c170ef00995f6c1c18a0d0e71d79429420375
Larysa Yavorska <nottombraider@Larysas-MacBook-Air.local>
2021-02-26 18:33:41 +0100

    fix: create correct screen layout and sizes

--- src/Layouts/DefaultLayout.tsx
+++ src/Layouts/DefaultLayout.tsx
@@ -1,10 +1,11 @@
 import { Header } from "Components/Header";
 import { Footer } from "Components/Footer";
 import { PropsWithChildren } from "react";
+import "../styles/utility-styles.css";
 
 export const DefaultLayout = ({ children }: PropsWithChildren<unknown>) => {
   return (
-    <div className="flag-quiz-game-wrapper">
+    <div className="flag-quiz-game-wrapper flex column space-between">
       <Header />
       {children}
       <Footer />
