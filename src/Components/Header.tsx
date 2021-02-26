fb0524f0a4284d9dc3179019d312183e0da0da4d
Larysa Yavorska <nottombraider@Larysas-MacBook-Air.local>
2021-02-26 18:27:05 +0100

    refactor: update header and add new styles

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import AudioPlayControlButton from "Pages/StartScreen/AudioPlayControlButton";
 import "../styles/utility-styles.css";
-import "./styles-Header.css";
+import "./style-Header.css";
 import { navigate } from "@reach/router";
 import { RoutPaths } from "Pages/routes";
 
@@ -9,15 +9,13 @@ const AUDIO_CONTROL_ICONS: string[] = ["music", "sounds"];
 
 export const Header = () => {
   return (
-    <header className="header flex align-center space-around ">
+    <header className="header flex align-center space-between">
       <div
         className="header__logo"
         onClick={() => navigate(RoutPaths.StartScreen)}
       >
         <span className="header__sub-text">one more</span>
-        <h1 className="header__text-logo">
-          Flag Quiz <span className="game-header-icon">&#127918;</span>
-        </h1>
+        <h1 className="header__text-logo">Flag Quiz</h1>
       </div>
 
       <ul className="header__button-container flex ">
