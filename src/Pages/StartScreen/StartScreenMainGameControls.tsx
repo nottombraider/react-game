b1665f3cee7977724e4935ab377541ecf06e8dec
Larysa Yavorska <nottombraider@Larysas-MacBook-Air.local>
2021-02-26 18:34:35 +0100

    feat: add styles to start game screen

--- src/Pages/StartScreen/StartScreenMainGameControls.tsx
+++ src/Pages/StartScreen/StartScreenMainGameControls.tsx
@@ -1,13 +1,21 @@
 import { useNavigate } from "@reach/router";
 import { RoutPaths } from "Pages/routes";
+import "./style-StartScreen.css";
 
 export const StartScreenMainGameControls = () => {
   const navigate = useNavigate();
 
   return (
-    <main className="start-screen-main flex column align-center space-around">
-      <button onClick={() => navigate(RoutPaths.GameScreen)}>Start Game</button>
-      {/* <button>Settings</button> */}
+    <main className="main-content-wrapper flex column align-center justify-center">
+      <div className="flex column">
+        <button
+          onClick={() => navigate(RoutPaths.GameScreen)}
+          className="main-game-button"
+        >
+          Start Game
+        </button>
+        <button className="main-game-button">Settings</button>
+      </div>
     </main>
   );
 };
