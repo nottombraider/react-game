13f8942ad0db2fb5067fcbba971745bcd523ee69
Larysa Yavorska <nottombraider@Larysas-MacBook-Air.local>
2021-02-26 18:26:39 +0100

    refactor: update footer and add basic styles

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,24 +1,26 @@
+import "./style-Footer.css";
+import footerLogo from "../pictures/nottombraider-github-logo.jpg";
+
 export const Footer = () => {
   return (
-    <footer className="flex space-between">
-      <div>
-        <span>2021 </span>
-        <a href="https://github.com/nottombraider/react-game">
-          <img
-            className="footer-img"
-            src="https://clipart.info/images/ccovers/1499794873github-logo-png.png"
-            alt="GitHub"
-          />{" "}
-          @nottombraider
-        </a>
-      </div>
-
-      <a href="https://community-z.com/events/react-rsschool-2021">
+    <footer className="footer flex space-between">
+      <a
+        href="https://community-z.com/events/react-rsschool-2021"
+        rel="noreferrer"
+        target="_blank"
+      >
         <img
-          className="footer-img"
+          className="footer-rsschool-logo"
           src="https://rs.school/images/rs_school_js.svg"
           alt="RS School logo"
         />
+      </a>
+      <a
+        href="https://github.com/nottombraider/react-game"
+        rel="noreferrer"
+        target="_blank"
+      >
+        <img className="footer-logo" src={footerLogo} alt="GitHub" />
       </a>
     </footer>
   );
