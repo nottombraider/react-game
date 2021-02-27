11ad89b0b88ab9b46d5c331dbae0bc7ff5109df0
Larysa Yavorska <nottombraider@Larysas-MacBook-Air.local>
2021-02-27 14:08:43 +0100

    fix: preloader layout

--- src/Components/Preloader.tsx
+++ src/Components/Preloader.tsx
@@ -5,10 +5,12 @@ import "../styles/utility-styles.css";
 
 export const Preloader = () => {
   return (
-    <div className="loading-screen flex column align-center">
-      <span className="loading-main-text">Waiting to start</span>
-      <div className="loading-circle"> </div>
-      <span className="loading-secondary-text">Loading...</span>
-    </div>
+    <DefaultLayout>
+      <div className="flex column align-center">
+        <span className="loading-main-text">Get ready</span>
+        <div className="loading-circle"> </div>
+        <span className="loading-secondary-text">Loading...</span>
+      </div>
+    </DefaultLayout>
   );
 };
