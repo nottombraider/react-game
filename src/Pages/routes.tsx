b075c4d827275f644341837e4a019f0359e0e0c0
Larysa Yavorska <nottombraider@Larysas-MacBook-Air.local>
2021-02-27 20:24:43 +0100

    feat: add win screen component 2

--- src/Pages/routes.tsx
+++ src/Pages/routes.tsx
@@ -1,5 +1,6 @@
 import { Router } from "@reach/router";
 import { GameOver } from "./EndScreens/GameOver";
+import { WinScreen } from "./EndScreens/WinScreen";
 import { MainGameScreen } from "./GameScreen/MainGameScreen";
 import { SettingsScreen } from "./Settings/SettingsScreen";
 import { StartScreen } from "./StartScreen/StartScreen";
@@ -8,6 +9,7 @@ export enum RoutPaths {
   StartScreen = "/",
   GameScreen = "/game",
   GameOver = "/game-over",
+  WinScreen = "/win-screen",
   SettingsScreen = "/settings",
 }
 
@@ -17,6 +19,7 @@ export const Routes = () => {
       <StartScreen path={RoutPaths.StartScreen} />
       <MainGameScreen path={RoutPaths.GameScreen} />
       <GameOver path={RoutPaths.GameOver} />
+      <WinScreen path={RoutPaths.WinScreen} />
       <SettingsScreen path={RoutPaths.SettingsScreen} />
     </Router>
   );
