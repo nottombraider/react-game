1a64bee689b6fba7e1c9bfcabc6e888f0b7a06aa
Larysa Yavorska <nottombraider@Larysas-MacBook-Air.local>
2021-03-03 00:01:39 +0100

    feat: use game screen in main game screen

--- src/Pages/GameScreen/MainGameScreen.tsx
+++ src/Pages/GameScreen/MainGameScreen.tsx
@@ -1,18 +1,13 @@
-import { FunctionComponent } from "react";
+import React, { FunctionComponent } from "react";
 import { RouteComponentProps } from "@reach/router";
-import { DefaultLayout } from "Layouts/DefaultLayout";
 import { useCountryFlags } from "./useCountryFlags";
-import { GameStepper } from "./GameStepper";
 import { Preloader } from "Components/Preloader";
 import "./main-game-screen.css";
+import { GameScreen } from "./GameScreen";
 
 export const MainGameScreen: FunctionComponent<RouteComponentProps> = () => {
   const [countryFlags, isLoading] = useCountryFlags();
 
   if (isLoading) return <Preloader />;
-  return (
-    <DefaultLayout>
-      <GameStepper countryFlags={countryFlags} />
-    </DefaultLayout>
-  );
+  return <GameScreen countryFlags={countryFlags} />;
 };
