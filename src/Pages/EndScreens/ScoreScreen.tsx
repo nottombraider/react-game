b3ee4c2e8bd07709885ee386e326fbccc2a2f315
nottombraider <larysa.yavorska@outlook.com>
2021-03-03 00:11:54 +0100

    refactor: fix ts error

--- src/Pages/EndScreens/ScoreScreen.tsx
+++ src/Pages/EndScreens/ScoreScreen.tsx
@@ -1,24 +1,25 @@
 import { FunctionComponent } from "react";
 import { DefaultLayout } from "Layouts/DefaultLayout";
-import { getDataFromLocalStorage } from "utils/getDataFromLocalStorage";
 import { RouteComponentProps } from "@reach/router";
+import { ScoreListType } from "types";
+import { getDataFromLocalStorage, StorageKeys } from "utils";
 
 export const ScoreScreen: FunctionComponent<RouteComponentProps> = () => {
-  const userScores = getDataFromLocalStorage("scores");
+  const userScores = getDataFromLocalStorage<ScoreListType>(
+    StorageKeys.Scores,
+    []
+  );
 
   return (
     <DefaultLayout>
       <div>
         <h1>Your scores:</h1>
         <ul>
-          {
-            //@ts-ignore
-            userScores.map((score) => (
-              <li>
-                {score.time} {score.score}
-              </li>
-            ))
-          }
+          {userScores.map((score) => (
+            <li>
+              {score.time} {score.score}
+            </li>
+          ))}
         </ul>
       </div>
     </DefaultLayout>
