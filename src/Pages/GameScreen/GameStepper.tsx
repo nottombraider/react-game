5387a1108d48a8606c9435591275c7975d2550b8
nottombraider <larysa.yavorska@outlook.com>
2021-03-03 20:22:13 +0100

    refactor: move some logic to other files

--- src/Pages/GameScreen/GameStepper.tsx
+++ src/Pages/GameScreen/GameStepper.tsx
@@ -1,80 +1,44 @@
-import { useState, MouseEvent, useEffect } from "react";
+import { useState, useEffect, MouseEvent } from "react";
 import { navigate } from "@reach/router";
 import { RoutPaths } from "Pages/routes";
-import { getVariants } from "utils";
-import { CountryFlags } from "./useCountryFlags";
 import arrayShuffle from "array-shuffle";
-import { getDataFromLocalStorage } from "utils/getDataFromLocalStorage";
+import { CurrentScoreType, CurrentVariants, GameStepperProps } from "types";
+import { DEFAULT_CURRENT_SCORE } from "Pages/EndScreens/GameOver";
+import { getStepCounter, getUpdatedUserFlags } from "gameHandlers";
+import {
+  getDataFromLocalStorage,
+  StorageKeys,
+} from "services/getDataFromLocalStorage";
 
-type GameStepperProps = {
-  countryFlags: CountryFlags;
-};
-
-export const GameStepper = ({ countryFlags }: GameStepperProps) => {
-  const [score, setScore] = useState<number>(0);
-  const [userFlags, setUserFlags] = useState(countryFlags);
-
-  const countryFlagsVariants = getVariants(userFlags, countryFlags, 3);
-  const [correctAnswer] = countryFlagsVariants;
-  const shuffledFlagsVariants = arrayShuffle(countryFlagsVariants);
+export const GameStepper = ({
+  userFlags,
+  setUserFlags,
+  correctVariant,
+  answerVariants,
+  allCountryFlagsLength,
+}: GameStepperProps) => {
+  const [score, setScore] = useState<CurrentScoreType>(() => {
+    return getDataFromLocalStorage<CurrentScoreType>(
+      StorageKeys.CurrentScore,
+      DEFAULT_CURRENT_SCORE
+    );
+  });
+  const [stepCounter, setStepCounter] = useState(() => getStepCounter());
 
   const handleChosenAnswer = (event: MouseEvent<HTMLButtonElement>) => {
     // @ts-ignore
     const chosenAnswer = event.target.innerText;
 
-    if (chosenAnswer === correctAnswer.name) {
-      const updatedUserFlags = [...userFlags];
-      const correctAnswerIndex = updatedUserFlags.indexOf(correctAnswer);
-      const seenFlagsJSON = localStorage.getItem("seenFlags");
-      const seenFlags: Array<number> = seenFlagsJSON
-        ? JSON.parse(seenFlagsJSON)
-        : [];
-
-      seenFlags.push(correctAnswer.id);
-      updatedUserFlags.splice(correctAnswerIndex, 1);
-
-      localStorage.setItem("seenFlags", JSON.stringify(seenFlags));
+    if (chosenAnswer === correctVariant.name.toUpperCase()) {
+      const updatedUserFlags = getUpdatedUserFlags(userFlags, correctVariant);
 
       setUserFlags(updatedUserFlags);
-      setScore(score + 10);
+      setScore({ time: new Date().toString(), score: score.score + 10 });
+      setStepCounter(stepCounter + 1);
     } else {
-      const scoresInLocalStorageJSON = localStorage.getItem("scores");
-      const scoresInLocalStorage = scoresInLocalStorageJSON
-        ? JSON.parse(scoresInLocalStorageJSON)
-        : [];
-
-      const userCurrentScore = getDataFromLocalStorage("currentScore");
-
-      if (scoresInLocalStorage.length < 10) {
-        scoresInLocalStorage.push(userCurrentScore);
-        //@ts-ignore
-        scoresInLocalStorage.sort((itemA, itemB) => itemA.score - itemB.score);
-        localStorage.setItem("scores", JSON.stringify(scoresInLocalStorage));
-      }
-
-      if (scoresInLocalStorage.length >= 10) {
-        //@ts-ignore
-        scoresInLocalStorage.sort((itemA, itemB) => itemA.score - itemB.score);
-        scoresInLocalStorage.splice(0, 1, userCurrentScore);
-        //@ts-ignore
-        scoresInLocalStorage.sort((itemA, itemB) => itemA.score - itemB.score);
-        localStorage.setItem("scores", JSON.stringify(scoresInLocalStorage));
-      }
-
-      localStorage.removeItem("seenFlags");
-      localStorage.removeItem("currentVariants");
       navigate(RoutPaths.GameOver);
     }
   };
-
-  useEffect(() => {
-    const userCurrentScore = {
-      time: new Date(),
-      score: score,
-    };
-
-    localStorage.setItem("currentScore", JSON.stringify(userCurrentScore));
-  }, [score]);
 
   useEffect(() => {
     if (userFlags.length === 0) {
@@ -83,32 +47,59 @@ export const GameStepper = ({ countryFlags }: GameStepperProps) => {
   }, [userFlags.length]);
 
   useEffect(() => {
-    const currentVariants = {
-      correct: correctAnswer.id,
-      answerVariants: countryFlagsVariants.map((item) => item.id),
+    const userCurrentScore: CurrentScoreType = {
+      time: new Date().toString(),
+      score: score.score,
     };
-    localStorage.setItem("currentVariants", JSON.stringify(currentVariants));
-  }, [correctAnswer.id, countryFlagsVariants]);
+
+    localStorage.setItem(
+      StorageKeys.CurrentScore,
+      JSON.stringify(userCurrentScore)
+    );
+    localStorage.setItem(StorageKeys.StepCounter, JSON.stringify(stepCounter));
+  }, [score, stepCounter]);
+
+  useEffect(() => {
+    const currentVariants: CurrentVariants = {
+      correct: correctVariant.id,
+      answerVariants: answerVariants.map((item) => item.id),
+    };
+    localStorage.setItem(
+      StorageKeys.CurrentVariants,
+      JSON.stringify(currentVariants)
+    );
+  }, [correctVariant.id, answerVariants]);
+
+  const shuffledFlagsVariants = arrayShuffle(answerVariants);
 
   return (
     <main className="main-content-wrapper flex column align-center space-around">
-      <div>
-        {/*TODO - add border to flags*/}
+      <div className="game-info flex space-between">
+        <div>
+          <span>Score: </span>
+          <span>{score.score} points</span>
+          <div className="step-counter">{`${stepCounter}/${allCountryFlagsLength}`}</div>
+        </div>
+        <button
+          className="pause-button"
+          onClick={() => navigate(RoutPaths.StartScreen)}
+        >
+          Pause
+        </button>
+      </div>
+      <div className="main-flag-wrapper">
         <img
-          src={correctAnswer.flag}
+          src={correctVariant.flag}
           alt="flag"
           className="main-flag-img animate__animated animate__backInDown"
         />
-        <div>
-          <span>Score: </span>
-          <span>{score} points</span>
-        </div>
       </div>
 
-      <ul className="answer-wrapper flex">
+      <ul className="answer-wrapper flex wrap space-around">
         {shuffledFlagsVariants.map((countryFlagVariant) => (
-          <li key={countryFlagVariant.name}>
+          <li key={countryFlagVariant.name} className="answer-button-li">
             <button
+              className="answer-button"
               onClick={handleChosenAnswer}
               aria-label={countryFlagVariant.name}
             >
